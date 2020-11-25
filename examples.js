eae34a88b6c3d449a39e869cca3edd7c1d3429b2
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-11-25 12:52:37 -0500

    Implemented minimal functionality plus sliding movement

---
+++ examples.js
@@ -0,0 +1,11 @@
+/* icy.js usage examples */
+const log = console.log
+
+"use strict";
+log('----------')
+log('SCRIPT: Examples of using our libraries')
+
+const icyGen = new IcyGenerator("slide");
+icyGen.makeIcy("circle");
+icyGen.makeIcy();
+icyGen.makeIcy();
