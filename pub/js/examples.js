5a9e058d1964d29d15e1033bf6e00f2364f24d16
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-11-26 11:58:44 -0500

    restructured folder

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -34,8 +34,8 @@ function deployInvasiveAds(e) {
         const tri = icyGen.makeIcy("triangle", ["800","50","50","50"])
         const rect = icyGen.makeIcy("rectangle", ["400","485","300","300"]);
 
-        circ.style.backgroundImage="url(./hardcoded/ad_one.jpg)"
-        rect.style.backgroundImage="url(./hardcoded/ad_two.jpg)"
+        circ.style.backgroundImage="url(./js/hardcoded/ad_one.jpg)"
+        rect.style.backgroundImage="url(./js/hardcoded/ad_two.jpg)"
 
         icyGen.setRandomMovement(true) // RandomMovement is automatically disabled if you click on the icy element
         
---
+++ server.js
@@ -0,0 +1,19 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+// Setting up a static directory for the files in /pub
+// using Express middleware.
+// Don't put anything in /pub that you don't want the public to have access to!
+app.use(express.static(path.join(__dirname, '/pub')))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
