f821391027869b41e97bd4f4290c3d5301c82fa2
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-11-26 12:53:29 -0500

    heroku attempt 3

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const path = require('path');
 // Don't put anything in /pub that you don't want the public to have access to!
 log('Debug Message')
 app.use(express.static(path.join(__dirname, '/pub')))
+log('path: ' + path.join(__dirname + '/pub'))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
