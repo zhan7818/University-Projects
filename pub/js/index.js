c379260245810122e54ab929e90ba6479cfc823d
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-12-19 15:45:08 -0500

    fixed url errors

--- pub/js/index.js
+++ pub/js/index.js
@@ -12,10 +12,10 @@ const icyGen = new IcyGenerator("slide","collide","soundEnabled");
 const title = icyGen.makeIcy("rectangle", ["500","100","900","500"],'./js/hardcoded/click.mp3','./js/hardcoded/slide.mp3', window.location.href)
 icyGen.setBackgroundImage("url(./js/hardcoded/icy2.jpg)", title)
 
-const examplesPath = window.location.href + "/examples.html"
+const examplesPath = window.location.href + "examples.html"
 const examplesIcy = icyGen.makeIcy("rectangle", ["700","650","200","100"],'./js/hardcoded/click.mp3','./js/hardcoded/slide.mp3', examplesPath)
 
-const apiPath = window.location.href + "/api.html"
+const apiPath = window.location.href + "api.html"
 const apiIcy = icyGen.makeIcy("rectangle", ["1000","650","200","100"],'./js/hardcoded/click.mp3','./js/hardcoded/slide.mp3', apiPath)
 
 icyGen.setBackgroundImage("url(./js/hardcoded/examples.jpg)", examplesIcy)
