4ee6be0f7f7bab7f9324f1eac2dfda1f6389634d
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-12-19 13:42:10 -0500

    heroku test 3

--- pub/js/index.js
+++ pub/js/index.js
@@ -13,6 +13,6 @@ const title = icyGen.makeIcy("rectangle", ["500","100","900","500"],'./js/hardco
 icyGen.setBackgroundImage("url(./js/hardcoded/icy2.jpg)", title)
 
 // const examplesPath = (window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) + "/examples.html")
-const examplesPath = window.location.href + "/examples.html"
+const examplesPath = window.location.href + "examples.html"
 const examplesIcy = icyGen.makeIcy("rectangle", ["500","650","200","100"],'./js/hardcoded/click.mp3','./js/hardcoded/slide.mp3', examplesPath)
 icyGen.setBackgroundImage("url(./js/hardcoded/examples.jpg)", examplesIcy)
